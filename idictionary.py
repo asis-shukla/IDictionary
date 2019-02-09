68cebc1f85c702f0306180b630e7cf55b9ec5cec
Ashish Shukla <ashishfun008@gmail.com>
2019-02-10 02:30:03 +0530

    remove redundant code and add precise version

--- idictionary.py
+++ idictionary.py
@@ -7,21 +7,16 @@ d_data = json.load(file_ptr)        # load json data to d_data
 
 while True:
     word = input("Enter your word: ")
-    if word == "" or word == " ":
-        print("This is blank, Please enter correct word! ")
-    else:
-        temp = False
-        word = word.lower()  # Convert string enttered by the user to all lower case 
-        for key in d_data:
-            if key == word:
-                temp = True   # Just to remember that word is found in the dictionary
-                # Now word is found in the dictionary, we print the each meaning as a sentence on the screen
-                for sentence in d_data[key]:
-                    print(sentence)         
-            else:
-                pass   # check word for another key
-        if not temp:
-            print(word,  "not found in the dictionary!, check for spelling error or enter another word!")
-        else:
-            pass
+    word = word.lower()  # Convert string enttered by the user to all lower case
+    
+    if word == "" or word[0:1] == " ":
+        print("This is not correct, Please enter correct word! ")
 
+    elif word in d_data.keys():
+        #word is found in the dictionary, we print the each meaning as a sentence on the screen
+        for sentence in d_data[word]:
+            print(sentence)         
+    
+    else:
+        print(word,  "not found in the dictionary!, check for spelling error or enter another word!")
+    
